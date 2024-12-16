97fde6ae8d4471e64201b865c6371b5621b4b8e4
GustavoDesk <rocha.gustavo@escolar.ifrn.edu.br>
2024-12-16 08:05:08 -0300

    up discount

--- OOP/Basic/Discount.php
+++ OOP/Basic/Discount.php
@@ -1,10 +1,33 @@
 <?php
 class Discount{
-    public $nome;
-    public $produto;
+    public $name;
+    public $price;
 
-    function __construct($nome,$produto){
-        $this->nome= $nome;
-        $this->produto= $produto;
+    function __construct($name,$price){
+        $this->name= $name;
+        $this->price= $price;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getPrice(){
+        return $this->price;
+    }
+    public function setName($name){
+        $this->nome=$name;
+    }
+    public function setPrice($price){
+        $this->price=$price;
+    }
+    public function makeDiscount($discountParam){
+        $originalPrice = $this->price;
+        $name = $this->name;
+        $priceWithDiscount = $originalPrice * (1-$discountParam); 
+        echo  $name.' preço é '.$originalPrice.' com desconto de '.$discountParam. 'O preço fica '. $priceWithDiscount;
+        return $priceWithDiscount.' '.$name;
     }
 }
+
+$teste = new Discount("Sapato", 10.00);
+$check=$teste->makeDiscount(0.20);
+echo $check;
